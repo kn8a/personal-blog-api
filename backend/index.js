ef86023b2e74928888de8a8b51115f745f1f114c
kn8a <88045655+kn8a@users.noreply.github.com>
2022-07-21 14:23:11 +0700

    use cors

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,16 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 3000
 const connectDb = require('./config/db')
+const cors = require('cors')
 const app = express()
 
 connectDb()
 //add body handler (form data)
+app.use(cors());
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
 
 app.use('/api/posts', require('./routes/postsRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
